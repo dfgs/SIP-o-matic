044aa9ec002db9cb4568e3566d84941b4fbedcd7
dfgs <dfgs83@gmail.com>
2022-09-02 11:58:21 +0200

    UI refactoring

---
+++ SIP-o-matic/Views/PropertyView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIP_o_matic.Views
+{
+	/// <summary>
+	/// Logique d'interaction pour PropertyView.xaml
+	/// </summary>
+	public partial class PropertyView : UserControl
+	{
+
+
+		public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(PropertyView));
+		public string Header
+		{
+			get { return (string)GetValue(HeaderProperty); }
+			set { SetValue(HeaderProperty, value); }
+		}
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(PropertyView));
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+
+
+
+		public PropertyView()
+		{
+			InitializeComponent();
+		}
+	}
+}
